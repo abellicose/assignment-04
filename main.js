f04114ea1184a0311b127d530a6544bea2afe2e8
Zubayer Hossain <zubayerhossain180@gmail.com>
2026-02-22 17:39:47 +0600

    I think it's working now :D

--- main.js
+++ main.js
@@ -1,75 +1,82 @@
+const trackerData = document.getElementsByTagName("dd");
+const visibleJobs = document.getElementById("jobs_visible");
 const tabs = document.getElementById("tabs");
-const idx = {"all": 0, "interview": 1, "rejected": 2};
-const panels = document.getElementsByClassName("tab_panels");
-const jobCounter = document.getElementsByClassName("jobs_visible")[0];
-let selectedPanel = "all";
+const tabBtn = tabs.children;
 
-tabs.addEventListener("click", (e) => {
-    if (e.target == e.currentTarget) {console.log("returned"); return};
-    for (const tab of tabs.children) {
-        tab.classList.remove("tab_selected");
+const panels = document.getElementsByClassName("tab_panels");
+const allJobs = panels[0].children;
+const allInterview = panels[1].children;
+const allRejected = panels[2].children;
+
+const idx = {"all": 0, "interview": 1, "rejected": 2};
+let selectedIdx = 0;
+
+tabs.addEventListener("click", function(e){
+    if (e.target === e.currentTarget) return;
+
+    selectedIdx = idx[e.target.innerText.toLowerCase()];
+
+    for (const btn of tabBtn) {
+        btn.classList.remove("tab_selected");
     }
+
     e.target.classList.add("tab_selected");
-    selectedPanel = e.target.innerText.toLowerCase();
-    updateVisiblePanel();
+    updateVisibleJobs();
 });
 
-// updating cards
-
-const updateForms = document.getElementsByClassName("update_form");
-
-for (const form of updateForms) {
-    form.addEventListener("click", (e) => {
-        if (e.target == e.currentTarget) return;
-        e.preventDefault();
-        const mainElement = e.target.closest("li");
-        const clonedElement = e.target.closest("li").cloneNode(true);
-        updateStatus(mainElement, e.target.className);
-        updateStatus(clonedElement, e.target.className);
-        panels[idx[e.target.className]].appendChild(clonedElement);
-        updateTracker();
-        updateVisiblePanel();
-    });
-}
-
-// deleting cards
-
-const deleteForms = document.getElementsByClassName("delete_form");
-for (const form of deleteForms) {
-    form.addEventListener("click", (e) => {
-        if (e.target === e.currentTarget) return;
-        event.preventDefault();
-        e.target.closest("li").remove();
-        updateTracker();
-        updateVisiblePanel();
-    });
-}
-
-const trackerData = document.getElementsByTagName("dd");
-function updateTracker() {
-    for (let i = 0; i < 3; i++) {
-        trackerData[i].innerText = panels[i].childElementCount;
-    }
-}
-
-function updateVisiblePanel() {
+function updateVisibleJobs() {
     for (const panel of panels) {
         panel.classList.add("hidden");
     }
 
-    const i = idx[selectedPanel];
-    const length = panels[i].childElementCount;
+    const len = panels[selectedIdx].childElementCount;
+    panels[len != 0 ? selectedIdx : 3].classList.remove("hidden");
 
-    jobCounter.innerText = `${length} Jobs`;
-
-    panels[length == 0 ? 3 : i].classList.remove("hidden");
+    visibleJobs.innerText = `${len} Jobs`;
 }
 
-function updateStatus(element, newStatus) {
-    const status = element.getElementsByTagName("output")[0];
-    for (const st of status.classList) {
-        if(st.startsWith("status_")) status.classList.remove(st)
-    }
-    status.classList.add(`status_${newStatus}`);
-    status.innerText = newStatus;
+const updateForms = document.getElementsByClassName("update_form");
+const deleteForms = document.getElementsByClassName("delete_form");
+
+for (const panel of panels) {
+    panel.addEventListener("click", function(e) {
+        if (e.target.matches(".update_form button") || e.target.matches(".delete_form button")){
+            e.preventDefault();
+            const value = e.target.value;
+            const id = e.target.closest("li").dataset.id;
+            let job;
+            for (const j of allJobs) {
+                if (j.dataset.id == id) {
+                    job = j;
+                    break;
+                }
+            }
+
+            const statusVal = e.target.matches(".delete_form button") ? "Not Applied" : value;
+            job.getElementsByClassName("job_status")[0].outerHTML = `<output class="job_status status_${statusVal}">${statusVal}</output>`;
+
+            for (const j of allInterview) {
+                if (j.dataset.id == id) j.remove();
+            }
+            for (const j of allRejected) {
+                if (j.dataset.id == id) j.remove();
+            }
+
+            if (e.target.matches(".update_form button")) {
+                panels[idx[value]].appendChild(job.cloneNode(true));
+            } else if (selectedIdx == 0) {
+                for (const j of allJobs) {
+                    if (j.dataset.id == id) j.remove();
+                }
+            }
+            updateVisibleJobs();
+            updateTrackerData();
+        }
+    });
+}
+
+function updateTrackerData() {
+    trackerData[0].innerText = allJobs.length;
+    trackerData[1].innerText = allInterview.length;
+    trackerData[2].innerText = allRejected.length;
 }
